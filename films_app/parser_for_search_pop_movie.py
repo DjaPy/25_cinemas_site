8dc20979241e599771756851ecbc649d35bcaeae
Ivanov Sergey <djapy@yandex.ru>
2017-11-28 14:06:26 +0300

    Add the use of threads to speed up reading web pages

--- films_app/parser_for_search_pop_movie.py
+++ films_app/parser_for_search_pop_movie.py
@@ -1,11 +1,17 @@
 from datetime import datetime, timedelta
 import re
 import requests
-from multiprocessing.dummy import Pool
+import time
+from queue import Queue
+import threading
 from bs4 import BeautifulSoup as bs
 
 
-IMAGE_FILM_URL = 'http/kinopoisk.ru/images/film_big/{}.jgp'
+IMAGE_FILM_URL = 'https://st.kp.yandex.net/images/{}/817969.jpg'
+URL_AFISHA = 'https://www.afisha.ru/msk/schedule_cinema/'
+URLS_KINOPOISK = ['https://www.kinopoisk.ru/premiere/ru/2017/month/{}/',
+                  'https://www.kinopoisk.ru/premiere/ru/2017/month/{}/page/1/ajax/true/']
+queue = Queue()
 
 
 def get_date_for_search(delta_days):
@@ -14,71 +20,21 @@ def get_date_for_search(delta_days):
     return current_date, initial_date
 
 
-def fetch_afisha_page():
-    url_afisha = 'https://www.afisha.ru/msk/schedule_cinema/'
-    raw_html = requests.get(url_afisha).content
-    return raw_html
+def fetch_movie_kinopoisk(queue, url):
+    queue.put(requests.get(url).content)
 
 
-def fetch_afisha_film_page(link_movie):
-    content_film_page = requests.get(link_movie).content
-    return content_film_page
-
-
-def parse_afisha_film_page(link_movie):
-    content = bs(fetch_afisha_film_page(link_movie), 'html.parser')
-    description_id = {'id':'ctl00_CenterPlaceHolder_ucMainPageContent_pEditorComments'}
-    ganres = content.find_all('div', {'class':'b-tags'}, 'a')
-    try:
-        movie_description = content.find('p', description_id).text.strip()
-    except AttributeError:
-        movie_discription = None
-    return movie_description, ganres[0].text.strip()
-
-
-def parse_afisha_list(afisha_html, good_count_cinemas):
-    content = bs(afisha_html, 'html.parser')
-    content_movies = content('div',
-                             {'class': 'object s-votes-hover-area collapsed'})
-    info_movies_afisha = []
-    for movie in content_movies:
-        title = movie('h3', {'class': 'usetags'})[0].get_text()
-        count_cinemas = len(movie('td',{'class': 'b-td-item'},'a'))
-        link_movie = movie.h3.a['href']
-        if int(count_cinemas) >= good_count_cinemas:
-            description, ganres = parse_afisha_film_page(link_movie)
-            title_and_count_cinemas = {'title': title,
-                                       'count_cinemas': count_cinemas,
-                                       'ganres': ganres,
-                                       'description': description}
-            info_movies_afisha.append(title_and_count_cinemas)
-    return info_movies_afisha
-
-
-def fetch_movie_info(last_month, current_month):
-    url_premiers_of_month = 'https://www.kinopoisk.ru/premiere/ru/2017/month/{}/'
-    url_after_scroll = 'page/1/ajax/true/'
-    if current_month != last_month:
-        last_month_content = requests.get(
-            url_premiers_of_month.format(last_month)).content
-        last_month_content_scroll = requests.get(
-            url_premiers_of_month.format(last_month) +
-            url_after_scroll).content
-        current_month_content = requests.get(
-            url_premiers_of_month.format(current_month)).content
-        current_month_content_scroll = requests.get(
-            url_premiers_of_month.format(current_month) +
-            url_after_scroll).content
-        full_content = last_month_content + current_month_content + \
-                       current_month_content_scroll + last_month_content_scroll
-    else:
-        current_month_content = requests.get(
-            url_premiers_of_month.format(current_month)).content
-        current_month_content_scroll = requests.get(
-            url_premiers_of_month.format(current_month) +
-            url_after_scroll).content
-        full_content = current_month_content + current_month_content_scroll
-    return full_content
+def get_threads_kinopoisk(month):
+    content_list = []
+    for url in URLS_KINOPOISK:
+        url = url.format(month)
+        thread = threading.Thread(target=fetch_movie_kinopoisk,
+                                  args=(queue, url))
+        thread.start()
+        result_thread = queue.get()
+        content_list.append(result_thread)
+    content = content_list[0] + content_list[1]
+    return content
 
 
 def get_films_in_kinopoisk(kinopoisk_content,
@@ -107,6 +63,65 @@ def get_films_in_kinopoisk(kinopoisk_content,
     return info_movies_kinopoisk
 
 
+def fetch_afisha_film():
+    raw_html = requests.get(URL_AFISHA).content
+    return raw_html
+
+
+def parse_afisha_schedule_cinema(afisha_html, good_count_cinemas):
+    content = bs(afisha_html, 'html.parser')
+    content_movies = content('div',
+                             {'class': 'object s-votes-hover-area collapsed'})
+    info_movies_afisha = []
+    for movie in content_movies:
+        count_cinemas = len(movie('td',{'class': 'b-td-item'},'a'))
+        if int(count_cinemas) >= good_count_cinemas:
+            info_movies_afisha.append({'count_cinemas': count_cinemas,
+                                       'movie_content': movie})
+    return info_movies_afisha
+
+
+def parse_info_movies_afisha(info_movies_afisha):
+    data_info_movies = []
+    for movie in info_movies_afisha:
+        count_cinemas = movie['count_cinemas']
+        movie = movie['movie_content']
+        title = movie('h3', {'class': 'usetags'})[0].get_text()
+        link_movie = movie.h3.a['href']
+        film_info = {'title': title,
+                      'count_cinemas': count_cinemas,
+                      'link': link_movie}
+        data_info_movies.append(film_info)
+    return data_info_movies
+
+
+def fetch_afisha_film_page(queue, link_movie):
+    queue.put(requests.get(link_movie).content)
+
+
+def get_threads_afisha(data_info_movies):
+    movie_full_info_with_afisha = []
+
+    for movie in data_info_movies:
+        url = movie['link']
+        thread = threading.Thread(target=fetch_afisha_film_page,
+                                  args=(queue, url))
+        thread.start()
+        content = queue.get()
+        content = bs(content, 'html.parser')
+        description_id = {
+            'id': 'ctl00_CenterPlaceHolder_ucMainPageContent_pEditorComments'}
+        ganres = content.find_all('div', {'class': 'b-tags'}, 'a')
+        try:
+            movie_description = content.find('p', description_id).text.strip()
+        except AttributeError:
+            movie_discription = None
+        movie.update(ganres=ganres[0].text.strip(), description=movie_description)
+        del movie['link']
+        movie_full_info_with_afisha.append(movie)
+    return movie_full_info_with_afisha
+
+
 def get_pop_movies(list_afisha, list_kinopoisk):
     common_info_list = []
     for movie_afisha in list_afisha:
@@ -114,27 +129,59 @@ def get_pop_movies(list_afisha, list_kinopoisk):
             if movie_afisha['title'] == movie_kinopoisk['title']:
                 movie_afisha.update(movie_kinopoisk)
                 common_info_list.append(movie_afisha)
-    print(common_info_list)
     return common_info_list
 
 
 def output_movies():
+    delta_days = 30
+    good_rate = 5.00
+    good_count_cinemas = 30
+    current_date, initial_date = get_date_for_search(delta_days)
+    current_month = datetime.today().month
+    last_month = initial_date.month
+    if last_month != current_month:
+        kinopoisk_content = get_threads_kinopoisk(last_month) + \
+                             get_threads_kinopoisk(current_month)
+    else:
+        kinopoisk_content = get_threads_kinopoisk(current_month)
+    kinopoisk_info_list = get_films_in_kinopoisk(kinopoisk_content,
+                                                initial_date,
+                                                current_date,
+                                                good_rate)
+    afisha_html = fetch_afisha_film()
+    info_movies_afisha = parse_afisha_schedule_cinema(afisha_html,
+                                                       good_count_cinemas)
+    data_movies_afisha = parse_info_movies_afisha(info_movies_afisha)
+    afisha_info_list = get_threads_afisha(data_movies_afisha)
+    return get_pop_movies(afisha_info_list, kinopoisk_info_list)[:10]
+
+
+if __name__ == '__main__':
     delta_days = 31
     good_rate = 5.00
     good_count_cinemas = 30
     current_date, initial_date = get_date_for_search(delta_days)
     current_month = datetime.today().month
     last_month = initial_date.month
-    kinopoisk_content = fetch_movie_info(last_month, current_month)
-    list_kinopoisk = get_films_in_kinopoisk(kinopoisk_content,
-                                            initial_date,
-                                            current_date,
-                                            good_rate)
-    afisha_content = fetch_afisha_page()
-    list_afisha = parse_afisha_list(afisha_content, good_count_cinemas)
-    common_info_list = get_pop_movies(list_afisha, list_kinopoisk)
-    return common_info_list[:10]
-
-
-if __name__ == '__main__':
-    print(output_movies())
+    tic = time.time()
+    if last_month != current_month:
+        kinopoisk_content = get_threads_kinopoisk(last_month) +\
+                            get_threads_kinopoisk(current_month)
+    else:
+        kinopoisk_content = get_threads_kinopoisk(current_month)
+    kinopoisk_info_list = get_films_in_kinopoisk(kinopoisk_content,
+                                                 initial_date,
+                                                 current_date,
+                                                 good_rate)
+    tac = time.time()
+    print(tac - tic)
+    tic = time.time()
+    afisha_html = fetch_afisha_film()
+    info_movies_afisha = parse_afisha_schedule_cinema(afisha_html,
+                                                     good_count_cinemas)
+    data_movies_afisha = parse_info_movies_afisha(info_movies_afisha)
+    afisha_info_list = get_threads_afisha(data_movies_afisha)
+    tac = time.time()
+    print(tac - tic)
+    for movie in get_pop_movies(afisha_info_list, kinopoisk_info_list)[:10]:
+        print(movie)
