f7af05d6ff1902ed781ace9260ce0166c3668b3f
Ivanov Sergey <djapy@yandex.ru>
2017-11-28 23:47:16 +0300

    Create API

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import tempfile
 from werkzeug.contrib.cache import FileSystemCache
 from films_app.parser_for_search_pop_movie import output_movies
+import json
+import logging
 
 
 ONE_DAY = 60 * 60 * 24
-
-DEBUG = True
 
 app = Flask(__name__)
 tmp_dir = tempfile.mkdtemp()
@@ -24,6 +24,14 @@ def get_films_from_cache():
 @app.route('/')
 def films_list():
     return render_template('films_list.html', films=get_films_from_cache())
+
+
+@app.route('/api')
+def get_api():
+    return Response(json.dumps(get_films_from_cache(),
+                               indent=2,
+                               ensure_ascii=False),
+                    content_type='application/json; charset=utf-8')
 
 
 if __name__ == "__main__":
